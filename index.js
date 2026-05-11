82f64c37e0ad90ddcf2ab20adb41a461ad2cb9f8
AforArdi <binsbro36@gmail.com>
2026-05-11 15:50:51 +0600

    mongodb logged in

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
+const uri = process.env.MONGODB_URI
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Welcome from Home');
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
